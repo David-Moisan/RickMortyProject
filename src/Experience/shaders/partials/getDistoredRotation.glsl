4f3c485f54c27f412470fbab9bb49f6217eb41ba
David-Moisan <dmoisan.23@gmail.com>
2022-04-11 15:07:20 +0200

    Change: Portal > Add white dots

---
+++ src/Experience/shaders/partials/getDistoredRotation.glsl
@@ -0,0 +1,8 @@
+vec2 getDistoredRotation(vec2 v, float a) {
+    float s = sin(a);
+    float c = cos(a);
+    mat2 m = mat2(c, -s, s, c);
+    return m * v;
+}
+
+#pragma glslify: export(getDistoredRotation)
--- src/Experience/shaders/portalMain/fragment.glsl
+++ src/Experience/shaders/portalMain/fragment.glsl
@@ -4,16 +4,11 @@ uniform float uTime;
 uniform vec3 uColor1;
 uniform vec3 uColor2;
 uniform vec3 uColor3;
+uniform vec3 uColor4;
 
 #pragma glslify: getPerlinNoise2d = require('../partials/getPerlinNoise2d')
 #pragma glslify: getPerlinNoise3d = require('../partials/getPerlinNoise3d')
-
-vec2 rotate(vec2 v, float a) {
-    float s = sin(a);
-    float c = cos(a);
-    mat2 m = mat2(c, -s, s, c);
-    return m * v;
-}
+#pragma glslify: getDistoredRotation = require('../partials/getDistoredRotation')
 
 varying vec2 vUv;
 
@@ -31,7 +26,7 @@ void main() {
     }
 
     //FirstMix
-    vec2 distoredUv1 = rotate(centeredUv, distanceToCenter * 6.0);
+    vec2 distoredUv1 = getDistoredRotation(centeredUv, distanceToCenter * 6.0);
     float mix1 = getPerlinNoise3d(vec3(distoredUv1 * 20.0, uTime * 0.0005));
     mix1 += getPerlinNoise3d(vec3(distoredUv1 * 8.0, uTime * 0.0001));
     mix1 -= distanceToCenter * 6.0;
@@ -39,12 +34,19 @@ void main() {
     vec3 color = mix(uColor1, uColor2, mix1);
 
     //SecondMix
-    vec2 distoredUv2 = rotate(centeredUv, distanceToCenter * 4.0);
+    vec2 distoredUv2 = getDistoredRotation(centeredUv, distanceToCenter * 4.0 - uTime * 0.0001);
     float mix2 = getPerlinNoise3d(vec3(distoredUv2 * 20.0, uTime * 0.0005));
     mix2 += getPerlinNoise3d(vec3(centeredUv * M_PI, uTime * 0.0001));
     mix2 += pow(abs((distanceToCenter - 0.25) * 4.0), 2.0);
     mix2 = step(0.4, mix2);
     color = mix(color, uColor3, mix2);
+
+    //ThirdMix
+    vec2 distoredUv3 = getDistoredRotation(centeredUv, -uTime * 0.00002);
+    float mix3 = getPerlinNoise3d(vec3(distoredUv3 * 25.0, uTime * 0.0001));
+    mix3 += (distanceToCenter - 0.3) * 1.0;
+    mix3 = step(0.6, mix3);
+    color = mix(color, uColor4, mix3);
 
     gl_FragColor = vec4(color, 1.0);
 }
